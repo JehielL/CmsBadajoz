11222c0ae6c010665062859c93b41440ce04c748
JehielL <jehiel.py@gmail.com>
2024-12-09 16:24:54 +0100

    modificada url para logs .

--- src/app/home-sin-log/home-sin-log.component.ts
+++ src/app/home-sin-log/home-sin-log.component.ts
@@ -68,7 +68,7 @@ export class HomeSinLogComponent implements OnInit {
       password: this.loginForm.get('password')?.value ?? '',
     };
 
-    this.httpClient.post<Token>('https://localhost:8443/users/login', login).subscribe({
+    this.httpClient.post<Token>('https://213.165.74.6:8444/users/login', login).subscribe({
       next: (response) => {
         console.log(response.token);
         this.authService.saveToken(response.token);
