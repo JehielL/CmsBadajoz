797c9c7b24ddbfb2cfbb28e1fccd24d17089670f
JehielL <jehiel.py@gmail.com>
2024-12-29 23:00:07 +0100

    Agregados maps a details de eventos, pendiente en carrousel.

--- src/app/eventos-detail/eventos-detail.component.ts
+++ src/app/eventos-detail/eventos-detail.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { EventosBadajoz } from '../interfaces/eventosbadajoz.model';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import * as L from 'leaflet';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-eventos-detail',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, DatePipe],
   templateUrl: './eventos-detail.component.html',
   styleUrl: './eventos-detail.component.css'
 })
 export class EventosDetailComponent implements OnInit {
 
-
+  private map: any;
+  private mapInitialized = false;
   evento: EventosBadajoz | undefined;
 
   constructor(
@@ -33,6 +36,34 @@ export class EventosDetailComponent implements OnInit {
           console.error('Error al cargar los detalles de evento:', err);
         }
       });
+    }
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.evento && this.evento.latitude && this.evento.longitude && !this.mapInitialized) {
+      this.initMap(this.evento.latitude, this.evento.longitude);
+      this.mapInitialized = true;
+    }
+  }
+
+  private initMap(latitude: number, longitude: number): void {
+    const mapContainer = document.getElementById('map');
+    if (!mapContainer) {
+      console.error('Contenedor de mapa no encontrado.');
+      return;
+    }
+
+    // Inicializa el mapa en las coordenadas especificadas
+    this.map = L.map(mapContainer).setView([latitude, longitude], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 60,
+    }).addTo(this.map);
+
+    // Agrega un marcador al mapa
+    if (this.evento?.name) {
+      L.marker([latitude, longitude]).addTo(this.map)
+        .openPopup();
     }
   }
 }
